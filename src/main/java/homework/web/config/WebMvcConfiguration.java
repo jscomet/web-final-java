24cd8af841a2e72f01eafcdaa625aa8badb1b921
jscomet <3059741813@qq.com>
2024-12-10 13:33:14 +0800

    fix: 修复swagger文档查询失败

--- src/main/java/homework/web/config/WebMvcConfiguration.java
+++ src/main/java/homework/web/config/WebMvcConfiguration.java
@@ -66,8 +66,9 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         //需要为消息转换器设置一个对象转换器，对象转换器可以将Java对象序列化为json数据
         converter.setObjectMapper(new JacksonObjectMapper());
         //将自己的消息转化器加入容器中
-        converters.add(0, converter);
+        converters.add(6, converter);
     }
+
 
     /**
      * 注册自定义枚举类型转换器
